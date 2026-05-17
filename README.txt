467a572592dcdf0e92f59f8cad21f2f5a10db256
hoya3725 <hoya3725@gmail.com>
2026-05-17 12:11:34 +0900

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,13 @@
+희망플로우 GitHub Pages 업로드용 파일
+
+GitHub 저장소 hopeflow 안에 기존 파일을 지우고 아래 6개 파일만 업로드하세요.
+
+index.html
+manifest.webmanifest
+sw.js
+icon-192.png
+icon-512.png
+icon-1024.png
+
+업로드 후 GitHub Pages 주소:
+https://hoya3725.github.io/hopeflow/
---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "hopeflow-pwa-v4";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-1024.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
